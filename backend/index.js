feb33adca671159d2568b9cec928c1e06c2339b6
Blin <bk51836@ubt-uni.net>
2024-10-15 22:26:40 +0200

    CONNECTION WITH DATABASE

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 import express from "express"
+import mysql from "mysql"
 
 const app = express()
 
-app.listen(3306, ()=>{
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password:"admin",
+    database: "bookstore"
+})
+
+app.listen(3000, ()=>{
     console.log("Connected to backend!")
 })
