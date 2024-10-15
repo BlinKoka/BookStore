10ad6ef777e82b15966bd1ea9dc2889e43171cf9
Blin <bk51836@ubt-uni.net>
2024-10-15 22:42:53 +0200

    Start creating crud for books

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,32 @@ const db = mysql.createConnection({
     database: "bookstore"
 })
 
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.json("hello this is the backend")
+})
+
+app.get("/books", (req,res)=>{
+    const query = "SELECT * FROM books"
+    db.query(query,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req,res)=>{
+    const query = "INSERT INTO books ('title','desc','cover') VALUES (?)"
+    const values = [req.body.title,
+                    req.body.desc,
+                    req.body.cover]
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Book has been created successfully");
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("Connected to backend!")
 })
